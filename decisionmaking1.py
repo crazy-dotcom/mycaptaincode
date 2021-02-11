9b5816a031959156f0ed32d33e693c5fd2706109
crazy-dotcom <78637787+crazy-dotcom@users.noreply.github.com>
2021-02-11 17:25:17 +0530

    created
    created

---
+++ decisionmaking1.py
@@ -0,0 +1,17 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+i=0
+j=0
+n=0
+m=0
+while i<5:
+    i=i+1
+    if list1[n]>0:
+        print(list1[n])
+    n=n+1
+while j<4:
+    j=j+1
+    if list2[m]>0:
+        print(list2[m])
+    m=m+1
+    
