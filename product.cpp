e1cc09777fdbc8fbc70781ddc96220b37e435b5e
crazy-dotcom <78637787+crazy-dotcom@users.noreply.github.com>
2021-07-07 11:21:56 +0530

    product.cpp
    cpp file

---
+++ product.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  float x,y;
+  float product;
+  cout<<"Enter two numbers: ";
+  cin>>x;
+  cin>>y;
+  product=x*y;
+  cout<<"Product= "<<product;
+  return 0;
+}
+  
